8a862144a1d0a2b32a2ba9b22971acacf4fc0fa1
Francisco Gabriel <franciscogabrielf11@gmail.com>
2023-11-07 17:16:55 -0300

    feat: student repository

--- lib/data/student_repository.dart
+++ lib/data/student_repository.dart
@@ -1,4 +1,16 @@
 import 'package:challenge_flutter/domain/student_model.dart';
+import 'package:dio/dio.dart';
+
+class StudentRepository {
+  Future<List<Student>> getAllStudents() async {
+    final response = await Dio()
+        .get('https://653c0826d5d6790f5ec7c664.mockapi.io/api/v1/student');
+
+    return response.data
+        .map<Student>((student) => Student.fromMap(student))
+        .toList();
+  }
+}
 
 final List students = [
   Student(
