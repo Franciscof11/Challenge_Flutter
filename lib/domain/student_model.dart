528b3bf319425282d5071cdb2aeee0cae2e38d33
Francisco Gabriel <franciscogabrielf11@gmail.com>
2023-11-07 17:17:00 -0300

    feat: student model

--- lib/domain/student_model.dart
+++ lib/domain/student_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 class Student {
   final String createdAt;
@@ -16,4 +18,30 @@ class Student {
     required this.academicRecord,
     required this.id,
   });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'createdAt': createdAt,
+      'name': name,
+      'birthdate': birthdate,
+      'cpf': cpf,
+      'email': email,
+      'academicRecord': academicRecord,
+      'id': id,
+    };
+  }
+
+  String toJson() => jsonEncode(toMap());
+
+  factory Student.fromMap(Map<String, dynamic> map) {
+    return Student(
+      createdAt: map['createdAt'] ?? '',
+      name: map['name'] ?? '',
+      birthdate: map['birthdate'] ?? 0,
+      cpf: map['cpf'] ?? 0,
+      email: map['email'] ?? '',
+      academicRecord: map['academicRecord'] ?? 0,
+      id: map['id'] ?? '',
+    );
+  }
 }
