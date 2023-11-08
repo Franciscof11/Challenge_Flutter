1d13d8bab7954bcd67abc76876510589442d337f
Francisco Gabriel <franciscogabrielf11@gmail.com>
2023-11-08 09:41:42 -0300

    feat: loader bloc component

---
+++ lib/presentation/widgets/loader.dart
@@ -0,0 +1,27 @@
+import 'package:challenge_flutter/config/constant_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+//Bloc class -> Bloc Selector
+class Loader<B extends StateStreamable<S>, S> extends StatelessWidget {
+  final BlocWidgetSelector<S, bool> selector;
+
+  const Loader({super.key, required this.selector});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocSelector<B, S, bool>(
+      selector: selector,
+      builder: (context, loading) => Visibility(
+          visible: loading,
+          child: Column(
+            children: [
+              SizedBox(height: MediaQuery.of(context).size.height * 0.32),
+              const CircularProgressIndicator(
+                color: mainGreen,
+              ),
+            ],
+          )),
+    );
+  }
+}
